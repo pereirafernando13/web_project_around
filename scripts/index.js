0dc893e8814894487ca13fb613cc0917b86ec72f
pereriafernando13 <nando-psousa@outlook.com>
2024-09-01 22:20:56 +0100

    add js div popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,43 @@
-const test = document.querySelector(".profile__info-button-edit");
+const popup = document.querySelector(".popup");
+const editbutton = document.querySelector(".profile__info-button-edit");
+const closebutton = document.querySelector(".popup__button-close");
+
+const saveButtonProfile = document.querySelector(".popup__form-button-save");
+
+const form = document.querySelector(".popup__form");
+const inputName = form.querySelector("#name");
+const inputProfission = form.querySelector("#profission");
+
+const profileInfo = document.querySelector(".profile__info-name");
+const profileProfission = document.querySelector(".profile__info-profession");
+
+const buttonHeartLike = document.querySelector(
+  ".elements__element-button-heart"
+);
+const buttonHeartUnLike = document.querySelector(
+  ".elements__element-button-heart"
+);
+
+function openPopup() {
+  popup.classList.add("popup_change_display");
+}
+editbutton.addEventListener("click", openPopup);
+
+function closePopup() {
+  popup.classList.remove("popup_change_display");
+}
+closebutton.addEventListener("click", closePopup);
+
+function updateProfileInfo(event) {
+  event.preventDefault();
+  profileInfo.textContent = inputName.value;
+  profileProfission.textContent = inputProfission.value;
+  closePopup();
+}
+saveButtonProfile.addEventListener("click", updateProfileInfo);
+
+function heartLike() {
+  buttonHeartLike.classList.add("elements_element-button-heart-like");
+}
+
+buttonHeartLike.addEventListener("click", heartLike);
