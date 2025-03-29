3b77694a879bce1795be357c18a3148917d022da
pereriafernando13 <nando-psousa@outlook.com>
2025-03-29 13:50:42 +0000

    corrigido erros solicitados do revisor

--- scripts/Card.js
+++ scripts/Card.js
@@ -31,53 +31,33 @@ export default class Card {
       .querySelector(".elements__element-image")
       .setAttribute("src", this._link);
 
-    // const cardTrash = this._elementCard.querySelector(
-    //   "elements-element-button-trash"
-    // );
-
     this._setEventListeners();
 
-    // const elements = document.querySelector(".elements");
-    // elements.prepend(this._elementCard);
     // //botao like
-    // this._elementCard
-    //   .querySelector(".elements__element-button-heart")
-    //   .addEventListener("click", (event) => {
-    //     if (
-    //       event.target.getAttribute("src") ===
-    //       "./images/elements__image-heart-disble.png"
-    //     ) {
-    //       return event.target.setAttribute(
-    //         "src",
-    //         "./images/elements_element-button-heart-like.png"
-    //       );
-    //     }
-    //     return event.target.setAttribute(
-    //       "src",
-    //       "./images/elements__image-heart-disble.png"
-    //     );
-    //   });
+    this._elementCard
+      .querySelector(".elements__element-button-heart")
+      .addEventListener("click", (event) => {
+        if (
+          event.target.getAttribute("src") ===
+          "./images/elements__image-heart-disble.png"
+        ) {
+          return event.target.setAttribute(
+            "src",
+            "./images/elements_element-button-heart-like.png"
+          );
+        }
+        return event.target.setAttribute(
+          "src",
+          "./images/elements__image-heart-disble.png"
+        );
+      });
 
     // //remove card
-    // this._elementCard
-    //   .querySelector(".elements-element-button-trash")
-    //   .addEventListener("click", (event) => {
-    //     event.target.parentElement.remove();
-    //   });
-
-    //Abrir/fechar PopupImgFull
-    // const openImgFull = document.querySelector(".popup__imgfull-imgbig");
-    // const openImgFullTittle = document.querySelector(".popup__imgfull-tittle");
-
-    // this._elementCard
-    // .querySelector(".elements__element-image")
-    // .addEventListener("click", (event) => {
-    //   console.log(event.target);
-    //   openImgFull.setAttribute("alt", this._name);
-    //   openImgFull.setAttribute("src", this._link);
-    //   openImgFullTittle.textContent = this._name;
-    //   openFullImagPopup();
-    // });
+    this._elementCard
+      .querySelector(".elements-element-button-trash")
+      .addEventListener("click", (event) => {
+        event.target.parentElement.remove();
+      });
 
     return this._elementCard;
   }
